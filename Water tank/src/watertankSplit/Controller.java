83419db6905b1f21e1187769322e2e2ab09f7817
Germandrummer92 <Germandrummer92@gmail.com>
2015-06-11 14:13:38 +0200

    Final Version of Watertank Example

--- Water tank/src/watertankSplit/Controller.java
+++ Water tank/src/watertankSplit/Controller.java
@@ -9,18 +9,19 @@ package watertankSplit;
  *
  */
 public class Controller {
-	
 	private int state;
 	private int tick;
+	//private int old;
 	
 	public Controller() {
 		state = 0;
+		//old = 10;
 		tick = 30;
 	}
 	
 	/*@ public normal_behavior
-	  @ requires tick == 30 && y >= 10 && y <= 120 && (old == 10 || old == -20) && y + 2 * old <= 120 && y + 2 * old >= 10;
-	  @ ensures \result * (tick / 10) + y + 2 * old  >= 10 & \result * (tick / 10) + y + 2 * old <= 120;
+	  @ requires tick == 30 && y >= 10 && y <= 120 && y + 2 * old <= 120 && y + 2 * old >= 10 && (old == 10 || old == -20);
+	  @ ensures \result * (tick / 10) + y + 2 * old  >= 12 & \result * (tick / 10) + y + 2 * old <= 116 && (\result == 10 || \result == -20);
 	  @*/
 	public int getControlValue (int y, int old) {
 		//Waterlevel in two time units
@@ -28,10 +29,12 @@ public class Controller {
 		
 		//IF we are raising level, keep raising if possible without hitting max_level before next tick
 		if (old == 10) {
-			if (inTwo + tick * 1 <= 120) {
+			if (inTwo + tick * 1 <= 116) {
+			//	old = 10;
 				return 10;
 			}
 			else {
+				//	old = -20;
 					return -20;
 			}
 		}
@@ -39,16 +42,19 @@ public class Controller {
 		else {
 			if (old == -20) {
 		
-				if (inTwo - tick * 2 >= 10) {
+				if (inTwo - tick * 2 >= 12) {
+				//	old = -20;
 					return -20;
 				}
 				else {
+				//		old = 10;
 						return 10;
 				
 				}
 		
 			}
 		}
+		
 		//Only returned if old != 10 && old != -20, unreachable.
 		return 0;
 	}
